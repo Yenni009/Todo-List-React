278968f42e87b9c28060531e80cb9cb96592d254
Yenni <12345678+Yenni009@users.noreply.github.com>
2025-10-27 08:38:12 -0500

    deploy: disparar a gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ export default function App() {
       <h1 className="text-3xl font-bold mb-5 text-center">TODO LIST APP</h1>
       <div className="flex gap-3 mb-5">
         <input className="flex-1 p-2 border rounded" type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Añadir Tarea" />
-        <button className="bg-blue-500 text-white px-4 p-y-2 rounded" onClick={agregarTarea} >Añadir Tareas</button>
+        <button className="bg-blue-500 text-white px-4 p-y-2 rounded" onClick={agregarTarea} >Añadir Tarea</button>
       </div>
 
       <div className="space-y-2 ">
